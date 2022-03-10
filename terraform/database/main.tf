90733d54c885c17f52af7e68897fc7f7b9a9d469
Debian <admin@ip-172-31-32-124.eu-west-2.compute.internal>
2022-03-10 11:46:26 +0000

    new

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -77,10 +77,6 @@ resource "aws_s3_bucket" "data_bucket" {
   bucket = "${var.base_name}-data"
 }
 
-resource "aws_s3_bucket_acl" "data_bucket_acl" {
-  bucket = aws_s3_bucket.data_bucket.id
-  acl    = "private"
-}
 
 resource "null_resource" "docker_install" {
   connection {
